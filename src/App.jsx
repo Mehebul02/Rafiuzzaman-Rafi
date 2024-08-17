82cb206b4b5dd06aa11aba1b3089a0752b659a9a
Mehebul02 <mehebul2123@gmail.com>
2024-08-17 23:29:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="flex justify-center items-center text-4xl font-semibold min-h-screen">Rafiuzzaman Rafi</h1>
+    </>
+  )
+}
+
+export default App

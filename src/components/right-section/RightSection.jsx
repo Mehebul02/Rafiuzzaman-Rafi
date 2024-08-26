141121b04595fdf17ada41dd61d1673ec9d8496d
Mehebul02 <mehebul2123@gmail.com>
2024-08-26 20:29:17 +0600

    about add

---
+++ src/components/right-section/RightSection.jsx
@@ -0,0 +1,13 @@
+
+const RightSection = () => {
+    return (
+        <div className="bg-gray-300 w-full p-10 rounded-tl-xl rounded-br-xl">
+            <h1 className="text-3xl font-montserrat font-semibold">About me</h1>
+            <div className="divider"></div>
+            <p className="text-xl font-montserrat ">I want a stimulating teaching and research position where I can use my technical skills, creativity, and learning 
+            experiences to grow professionally and contribute to the organization`s success. </p>
+        </div>
+    );
+};
+
+export default RightSection;
--- src/roots/Roots.jsx
+++ src/roots/Roots.jsx
@@ -1,18 +1,20 @@
 import LeftPart from "../components/left-section/LeftPart";
+import RightSection from "../components/right-section/RightSection";
 import Container from "../shared/Container";
 
 const Roots = () => {
     return (
          <div className=" min-h-screen">
        <Container>
-          <div className="flex justify-between items-center gap-10 ">
+          <div className="grid grid-cols-2 gap-10 ">
              {/* Left Part   */}
            <div>
             <LeftPart/>
 
            </div>
           {/* Right part  */}
-          <div>
+          <div className="mt-36">
+            <RightSection/>
 
           </div>
           </div>

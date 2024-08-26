54c3958df5f877da092c1fdf1b12189cca9e27db
Mehebul02 <mehebul2123@gmail.com>
2024-08-26 23:20:25 +0600

    skills add

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,9 @@
 import personalImg from '../assets/images/profile.jpg'
 import msWord from '../assets/images/ms-word.png'
+import rlanguage from '../assets/images/RLanguage.png'
+import rStudio from '../assets/images/RStudio-Logo-All-Color-partners.png'
+import spss from '../assets/images/pngwing.com.png'
+import sas from '../assets/images/sas-logo-sas-blue.png'
+import stata from '../assets/images/logo-blue.png'
 
-export {personalImg,msWord}
+export {personalImg,msWord,rlanguage,rStudio,spss,sas,stata}
--- src/components/right-section/RightSection.jsx
+++ src/components/right-section/RightSection.jsx
@@ -1,7 +1,7 @@
 import { FcAbout } from "react-icons/fc";
 import { GiSkills } from "react-icons/gi";
-import { msWord } from "../../assets";
-import { FaFileWord } from "react-icons/fa";
+import { msWord, rlanguage, rStudio, sas, spss, stata } from "../../assets";
+import { FaFileWord, FaLaptopCode } from "react-icons/fa";
 import { SiMicrosoftaccess, SiMicrosoftexcel, SiMicrosoftpowerpoint } from "react-icons/si";
 
 const RightSection = () => {
@@ -18,27 +18,47 @@ const RightSection = () => {
             <div  className="bg-gray-300 w-full p-10 rounded-tl-xl rounded-br-xl ">
             <h1 className="text-3xl font-montserrat font-bold flex items-center gap-2">About My Skills <GiSkills className="text-[#4fb0ff] animate-bounce"/> </h1>
             <div className="divider"></div>
-          <div className="grid grid-cols-4">
-          {/* <img src={msWord} alt="ms-word" className="w-48"  />
-           <img src={msWord} alt="ms-word" className="w-48"  />
-           <img src={msWord} alt="ms-word" className="w-48"  />
-           <img src={msWord} alt="ms-word" className="w-48"  /> */}
-           {/* ms word  */}
+            <h1 className="text-xl  text-[#000000] font-serif flex items-center gap-2 my-5">Programming Skills <FaLaptopCode className="text-[#4fb0ff]"/> </h1> 
+            
+          <div className="grid grid-cols-4 gap-7">
+       {/* programming skills  */}
+           {/* R language  */}
+           <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+  <img src={rlanguage} alt="" />
+</div>
+           {/* R language  */}
+           <div className="w-32 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+  <img src={rStudio} alt="rstudio" className="mt-5" />
+</div>
+           {/* spss language  */}
+           <div className="w-32 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+  <img src={spss} alt="rstudio" className="mt-5" />
+</div>
+           {/* sas language  */}
+           <div className="w-32 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+  <img src={sas} alt="rstudio" className="mt-5" />
+</div>
+           {/* sas language  */}
+           <div className="w-32 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+  <img src={stata} alt="rstudio" className="mt-5" />
+</div>
+
+{/* ms word 
             <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
   <span><FaFileWord className="text-6xl text-blue-700 mx-auto"/> </span>
 </div>
            {/* ms excel  */}
-            <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+            {/* <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
   <span><SiMicrosoftexcel className="text-6xl text-blue-700 mx-auto"/> </span>
-</div>
+</div> */}
            {/* ms power point  */}
-            <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+            {/* <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
   <span><SiMicrosoftpowerpoint className="text-6xl text-blue-700 mx-auto"/> </span>
-</div>
+</div> */}
            {/* ms access  */}
-            <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
+            {/* <div className="w-28 bg-slate-300 hover:bg-slate-200 p-4 rounded-xl shadow-2xl transform ">
   <span><SiMicrosoftaccess className="text-6xl text-blue-700 mx-auto"/> </span>
-</div>
+</div>  */}
 
           </div>
         </div>
